f9a3f7d5538c161f827cf5cdbbccec5379e0fedf
Xing Voong <xingvoong@gmail.com>
2022-01-02 12:21:07 -0800

    init permutation

---
+++ practice_for_amazon/permutations/permutations.py
@@ -0,0 +1,9 @@
+'''
+Given an array nums of distint integers, return all the possible permutations.
+You can return the answer in any order
+
+example 1:
+input: nums = [1, 2, 3]
+output: [[1,2, 3], [1, 3, 2], [2, 1, 3], [3, 1, 2], [3, 2, 1]]
+
+'''
