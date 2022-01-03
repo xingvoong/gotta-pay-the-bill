72568c284225f5a064529e1b4e6f684a5f5fd052
Xing Voong <xingvoong@gmail.com>
2022-01-03 15:28:36 -0800

    init rorate right

---
+++ practice_for_amazon/rotate_list/rotate_right.py
@@ -0,0 +1,15 @@
+'''
+Given the head of a linked list, rotate the list to the right by k places
+
+example 1:
+
+input: head = [1, 2, 3, 4, 5], k = 2
+output: [4, 5, 1, 2, 3]
+
+example 2:
+
+input: head = [0, 1, 2], k = 4
+output: [2, 0, 1]
+'''
+
+def rorateRight(head, k):
