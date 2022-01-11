d5ae3c8c72ea786be37659bdc0c28c45c27379be
Xing Voong <xingvoong@gmail.com>
2022-01-11 11:46:07 -0800

    implement word_search.py

--- practice_for_amazon/word_search/word_search.py
+++ practice_for_amazon/word_search/word_search.py
@@ -1,4 +1,4 @@
-'''
+"""
 Given an mxn grid of characters board and a string word, return true if word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring.  The same letter cell may not be used more than once.
@@ -21,56 +21,73 @@ E:
 At each decision, I can do right, or go down.
 if there is a character, then I trigger the find?
 
-'''
+"""
+
 
 def exist(board, word):
-  """
-  :type board: List[ListListt[str]]
-  :type word: str
-  :rtype: bool
-  """
-  MAX_ROWS = len(board)
-  MAX_COLS = len(board[0])
+    """
+    :type board: List[ListListt[str]]
+    :type word: str
+    :rtype: bool
+    """
+    MAX_ROWS = len(board)
+    MAX_COLS = len(board[0])
+    path = set()
 
-  def backtracking(row, col, remain_word):
-    # if solution:
-    # output(solution)
-    if len(remain_word) == 0:
-      return True
+    def backtracking(row, col, i):
+        # if solution:
+        # output(solution)
+        if i == len(word):
+            return True
 
-    # visit all the next candidates
-    # which is the 4 neighbors
-    toReturn = False
-    for x in range(row - 1, row + 2):
-      for y in range(col - 1, col + 2):
-        if -1 < x < MAX_ROWS and -1 < y < MAX_COLS and x != row and y != col and board[x][y] == remain_word[0]:
-          print("x", x)
-          print("y", y)
-          print(remain_word[1:])
-          # place the next candidate
-          board[x][y] = "#"
+        # check to make sure the word begins is the same
+        # check if we are not out of bound
+        if (
+            row < 0
+            or row >= MAX_ROWS
+            or col < 0
+            or col >= MAX_COLS
+            or board[row][col] != word[i]
+            or (row, col) in path
+        ):
+            return False
 
-          # backtracking
-          # print(backtracking(x, y, remain_word[1:]))
+        # place a candidate
+        path.add((row, col))
 
-          toReturn = backtracking(x, y, remain_word[1:])
-          if toReturn:
-            break
+        # explore all the choices
+        res = (
+            backtracking(row - 1, col, i + 1)
+            or backtracking(row + 1, col, i + 1)
+            or backtracking(row, col + 1, i + 1)
+            or backtracking(row, col - 1, i + 1)
+        )
 
-          # revert the choice
-          board[x][y] = remain_word[0]
+        # revert the choice
+        path.remove((row, col))
+        return res
 
-          return toReturn
+    # loop through the board to check for where a work start
+    for row in range(MAX_ROWS):
+        for col in range(MAX_COLS):
+            if backtracking(row, col, 0):
+                return True
 
-  # loop through the board to check for where a work start
-  for row in range(MAX_ROWS):
-    for col in range(MAX_COLS):
-      if backtracking(row, col, word):
-        return True
+    return False
 
-  return False
 
-board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
 words = "ABCCED"
 
 print(exist(board, words))
+
+"""
+time:
+O(M x N x backtracking)
+
+backtracking: 4^(L) where L is the length of the word
+but we do not go back to where we come from so it is only 3^L
+
+space:
+O(L):
+"""
