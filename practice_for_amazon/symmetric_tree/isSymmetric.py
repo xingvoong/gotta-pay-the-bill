b1c21995babf8e2c47c140160acb3149cb5b84f5
Xing Voong <xingvoong@gmail.com>
2022-01-12 13:25:08 -0800

    init is symmetric tree

---
+++ practice_for_amazon/symmetric_tree/isSymmetric.py
@@ -0,0 +1,8 @@
+'''
+Given the root of binary tree, check whether it is a mirror of itself (i.em symetric around it center)
+
+Constraints:
+
+- the number of nodes in the tress is in the range [1, 1000].
+- -100 <= node.val <= 100
+'''
