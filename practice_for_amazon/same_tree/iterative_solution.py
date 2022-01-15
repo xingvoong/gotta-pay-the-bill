805558b5b70da1a2b71f684f1f34d72de4863d9c
Xing Voong <xingvoong@gmail.com>
2022-01-15 15:55:51 -0800

    implement iterative solution

--- practice_for_amazon/same_tree/iterative_solution.py
+++ practice_for_amazon/same_tree/iterative_solution.py
@@ -19,3 +19,94 @@ output: false
 - the number of nodes in both trees is in the range [0, 100]
 - -10^4 <= Node.val <= 10^4
 '''
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # insert into a binary tree
+    # recursively go to the leaf so that I can add something.
+    def insert(self, value):
+        # if the node is there with a value already
+        if self.value:
+            if value < self.value:
+                if self.left is None:
+                    self.left = TreeNode(value)
+                else:
+                    self.left.insert(value)
+            else:
+                if self.right is None:
+                    self.right = TreeNode(value)
+                else:
+                    self.right.insert(value)
+        # if the node is there but with no value
+        else:
+            self.value = value
+
+    # print tree
+    # visit the left
+    # print the node val
+    # visit the right
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.value)
+        if self.right:
+            self.right.PrintTree()
+
+    def isSameTree(self, p, q):
+
+      from collections import deque
+
+      def check(p, q):
+
+        if not p and not q:
+          return True
+
+        if not p or not q:
+          return False
+
+        if p.value != q.value:
+          return False
+
+        return True
+      # make a queue
+      # add node into it and then check the node with a helper function
+      # helper function check whether those 2 nodes are valid for the tree to be the same
+      queue = deque([(p, q)])
+
+      while queue:
+        p, q = queue.popleft()
+
+        if not check(p, q):
+          return False
+
+        if p:
+          queue.append((p.left, q.left))
+          queue.append((p.right, q.right))
+
+      return True
+
+
+p = TreeNode(1)
+p.insert(2)
+p.insert(3)
+
+q = TreeNode(1)
+q.insert(2)
+q.insert(3)
+
+print(p.isSameTree(p, q))
+
+
+
+'''
+# start from the root and then at each iteration pop the current node out of the dequeu.  Then do the same checks as in the approach 1:
+- p and q are not None
+- p.val is equal to q.val
+
+and if checks are ok, push the child nodes
+
+'''
--- practice_for_amazon/symmetric_tree/isSymmetric.py
+++ practice_for_amazon/symmetric_tree/isSymmetric.py
@@ -6,3 +6,103 @@ Constraints:
 - the number of nodes in the tress is in the range [1, 1000].
 - -100 <= node.val <= 100
 '''
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # insert into a binary tree
+    # recursively go to the leaf so that I can add something.
+    def insert(self, value):
+        # if the node is there with a value already
+        if self.value:
+            if value < self.value:
+                if self.left is None:
+                    self.left = TreeNode(value)
+                else:
+                    self.left.insert(value)
+            else:
+                if self.right is None:
+                    self.right = TreeNode(value)
+                else:
+                    self.right.insert(value)
+        # if the node is there but with no value
+        else:
+            self.value = value
+
+    # print tree
+    # visit the left
+    # print the node val
+    # visit the right
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.value)
+        if self.right:
+            self.right.PrintTree()
+
+    # in-order traversal, left, node, right
+    # create an empty list,
+    # add left node first followed by the root or parent node
+
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.value)
+            res += self.inorderTraversal(root.right)
+        return res
+
+    # pre-order traversal, node, left right
+
+    def preorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.value)
+            res += self.preorderTraversal(root.left)
+            res += self.preorderTraversal(root.right)
+        return res
+
+    # post-orde traversal, left, right, node
+    def postorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.postorderTraversal(root.left)
+            res += self.postorderTraversal(root.right)
+            res.append(root.value)
+        return res
+
+    def isSymmetric(self, root):
+
+      def isMirror(root1, root2):
+        if not root1 and not root1:
+          return True
+        if not root1 or not root2:
+          print("root1", root1)
+          print("root2", root2)
+          return False
+
+        print("root1.value", root1.value)
+        print("root2.value", root2.value)
+
+        return ((root1.value == root2.value) and
+                isMirror(root1.right, root2.left) and
+                isMirror(root1.left, root2.right))
+
+      return isMirror(root, root)
+
+
+
+
+root = TreeNode(1)
+root.insert(2)
+root.insert(2)
+# root.insert(3)
+# root.insert(4)
+# root.insert(4)
+# root.insert(3)
+
+print(root.inorderTraversal(root))
+print(root.isSymmetric(root))
